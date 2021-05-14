c384d3056b426d73d3fde2e5a8df7f743765b7ff
Elton Fonseca <eltonjuniorfonseca@gmail.com>
2021-05-14 16:41:57 -0300

    Added menu chose logic

--- lib/friends_app/cli/menu_choice.ex
+++ lib/friends_app/cli/menu_choice.ex
@@ -6,17 +6,46 @@ defmodule FriendsApp.CLI.MenuChoice do
     Shell.info("Escolha uma opção:")
 
     menu_itens = FriendsApp.CLI.MenuItems.all()
+    find_menu_item_by_index = &Enum.at(menu_itens, &1)
 
     menu_itens
-      |> Enum.map(&(&1.label))
-      |> display_options()
+    |> Enum.map(&(&1.label))
+    |> display_options()
+    |> generate_question()
+    |> Shell.prompt()
+    |> parse_answer()
+    |> find_menu_item_by_index.()
+    |> confirm_menu_item()
   end
 
   defp display_options(options) do
     options
-      |> Enum.with_index(1)
-      |> Enum.each(fn {option, index} ->
-        Shell.info("#{index} - #{option}")
-      end)
+    |> Enum.with_index(1)
+    |> Enum.each(fn {option, index} ->
+      Shell.info("#{index} - #{option}")
+    end)
+
+    options
+  end
+
+  defp generate_question(options) do
+    options = Enum.join(1..Enum.count(options), ",")
+    "Escolha uma opção: [#{options}]\n"
+  end
+
+  defp parse_answer(answer) do
+    {option, _} = Integer.parse(answer)
+    option - 1
+  end
+
+  defp confirm_menu_item(chosen_menu_item) do
+    Shell.cmd("clear")
+    Shell.info("Você escolheu a opção: #{chosen_menu_item.label}")
+
+    if Shell.yes?("Confirma?") do
+      Shell.info("... #{chosen_menu_item.label} ...")
+    else
+      start()
+    end
   end
 end
